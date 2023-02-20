c3f64e509f6f22cbe920a514f346a2f0bec8f682
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-20 17:05:36 +0530

    called the method to print the fibonacci series

--- src/main/java/com/niit/bej/Main.java
+++ src/main/java/com/niit/bej/Main.java
@@ -3,5 +3,6 @@ package com.niit.bej;
 public class Main {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
+        int fibonacciSeries = fibonacci.printFibonacciSeries(5);
     }
 }
